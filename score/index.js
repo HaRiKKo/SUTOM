a19ea6432d03c10aba88e5e9a7e52ed880a749a4
chleo <chevrierch@eisti.eu>
2022-10-08 20:49:55 +0200

    ajout du docker compose

--- score/index.js
+++ score/index.js
@@ -5,16 +5,16 @@ const os = require('node:os')
 const {LocalStorage} = require("node-localstorage")
 const app = express()
 //PORT=5000 node index.js // commande utiliser pour lancé l'app sur le port 5000
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 5000 
 //const port = 5000
 const path = "data/liste_francais_utf8.txt"
 const nbr_mots = 22740
 
-app.use(express.static('public'))
+app.use(express.static('../public'))
 
 app.get('/stat', (req, res) => {
-    var localStorage = new LocalStorage('../score/storage')
+    var localStorage = new LocalStorage('/storage')
     stat=JSON.parse((localStorage.getItem(0)))
     console.table(stat)
-    res.send(stat);
+    res.send(stat)
 })
